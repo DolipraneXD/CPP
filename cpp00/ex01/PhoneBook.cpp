652640a2f54d562e23fc2c1ca1cf01c029c8fc3a
Doliprane <elfathimohammedamine@gmail.com>
2024-10-12 11:39:40 +0100

    first

---
+++ cpp00/ex01/PhoneBook.cpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doliprane <doliprane@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/29 09:02:49 by moel-fat          #+#    #+#             */
+/*   Updated: 2024/10/03 14:22:48 by doliprane        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "phonebook.hpp"
+// Default constructor
+PhoneBook::PhoneBook()
+{
+    index = 0;
+}
+
+void PhoneBook::add() {
+    std::string firstname, lastname, nickname, phone_number, darkest_secret;
+
+    while (true) {
+        std::cout << "We are adding you to the PhoneBook. Please enter your first name:\n";
+        std::getline(std::cin, firstname);
+
+        if(std::cin.eof() || std::cin.bad())
+        {
+            std::cout << "end of line " << std::endl;
+            exit(1);
+        }
+        if (firstname.empty()) {
+            std::cout << "First name cannot be empty. Please enter again.\n";
+            continue;
+        }
+        std::cout << "Now your last name:\n";
+        std::getline(std::cin, lastname);
+        if (lastname.empty()) {
+            std::cout << "Last name cannot be empty. Please enter again.\n";
+            continue;
+        }
+        std::cout << "And your nickname:\n";
+        std::getline(std::cin, nickname);
+        if (nickname.empty()) {
+            std::cout << "Nickname cannot be empty. Please enter again.\n";
+            continue;
+        }
+        std::cout << "We will need your phone number:\n";
+        std::getline(std::cin, phone_number);
+        if (phone_number.empty()) {
+            std::cout << "Phone number cannot be empty. Please enter again.\n";
+            continue;
+        }
+        std::cout << "For sure you have some dark secret. Tell us:\n";
+        std::getline(std::cin, darkest_secret);
+        if (darkest_secret.empty()) {
+            std::cout << "Darkest secret cannot be empty. Please enter again.\n";
+            continue;
+        }
+        contacts[index].setFirstName(firstname);
+        contacts[index].setLastName(lastname);
+        contacts[index].setNickname(nickname);
+        contacts[index].setPhoneNumber(phone_number);
+        contacts[index].setDarkestSecret(darkest_secret);
+        index = (index + 1) % 8;
+        break;
+    }
+}
+
+
+void PhoneBook::search()
+{
+	int search_index;
+	std:: cout << "pls enter the index to search for :" << std::endl;
+	std:: cin >> search_index ;
+    std::cin.ignore(1024, '\n');
+    if (search_index < 0 || search_index >= 8)
+    {
+        std::cout << "Invalid index!" << std::endl;
+        return;
+    }
+
+    std::cout << "First Name: " << contacts[search_index].getFirstName() << std::endl;
+    std::cout << "Last Name: " << contacts[search_index].getLastName() << std::endl;
+    std::cout << "Nickname: " << contacts[search_index].getNickname() << std::endl;
+    std::cout << "Phone Number: " << contacts[search_index].getPhoneNumber() << std::endl;
+    std::cout << "Darkest Secret: " << contacts[search_index].getDarkestSecret() << std::endl;
+}
---
+++ cpp00/ex01/contact.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moel-fat <moel-fat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/29 09:02:52 by moel-fat          #+#    #+#             */
+/*   Updated: 2024/10/01 10:55:39 by moel-fat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+#include <iostream>
+
+#include <string>
+
+class Contact
+{
+	private:
+		std::string first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string phone_number;
+		std::string darkest_secret;
+
+	public:
+		// Setter declarations
+		void setFirstName(const std::string &fname);
+		void setLastName(const std::string &lname);
+		void setNickname(const std::string &nname);
+		void setPhoneNumber(const std::string &pnumber);
+		void setDarkestSecret(const std::string &secret);
+		// Getter declarations
+		std::string getFirstName() const;
+		std::string getLastName() const;
+		std::string getNickname() const;
+		std::string getPhoneNumber() const;
+		std::string getDarkestSecret() const;
+};
+
+#endif
