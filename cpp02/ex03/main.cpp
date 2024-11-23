00599bb73f4adb3080f4ff2b33281a9012695dbd
Mohammed Amine El Fathi <moel-fat@e1r13p3.1337.ma>
2024-11-23 15:59:50 +0100

    lol

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: moel-fat <moel-fat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 13:08:29 by moel-fat          #+#    #+#             */
-/*   Updated: 2024/11/23 14:41:50 by moel-fat         ###   ########.fr       */
+/*   Updated: 2024/11/23 14:48:04 by moel-fat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int main( void )
 	Point p2(11.0f, 1.0f);
 	Point p3(2.0f, 2.0f);
 	Point tofind(11.0f, 1.0f);
+	
 	if (bsp(p1, p2, p3, tofind) == true)
 		std::cout << "True"<< std::endl;
 	else
